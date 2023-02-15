f9764bdf3abae270b626a714574ac506a3ee08f0
Yashika <yashikabaan096@gmail.com>
2023-02-15 19:38:56 +0900

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `thebestnewswebsiteever`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [],
+}
