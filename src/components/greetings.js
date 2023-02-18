99dbb66ba0992a73aadf77a99d530cbdefe4c7a7
Yashika <yashikabaan096@gmail.com>
2023-02-18 12:38:07 +0900

    components and layouts

---
+++ src/components/greetings.js
@@ -0,0 +1,12 @@
+import Greeting from "../components/greeting";
+
+// Rendering the <Greeting> component
+const SayHello = () => {
+  return (
+    <div>
+      <Greeting name="Megan" />
+      <Greeting name="Obinna" />
+      <Greeting name="Generosa" />
+    </div>
+  );
+};
